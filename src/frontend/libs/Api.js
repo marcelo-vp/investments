eb465a0f00e853a95db4fca76904314a0b98fd9d
marcelo-vp <marcelos.mail@gmail.com>
2020-08-06 09:53:52 -0300

    Add API for the front end side

---
+++ src/frontend/libs/Api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+class Api {
+    constructor() {
+        this._base_path = '/investment';
+        this._timeout = 3000;
+    }
+
+    _get_http_client() {
+        if (!this._http_client) {
+            this._http_client = axios.create({
+                baseURL: this._base_path,
+                timeout: this._timeout,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        }
+        return this._http_client;
+    }
+
+    async match(url, data) {
+        return await this._get_http_client().post(url, data);
+    }
+}
+
+export default new Api();
