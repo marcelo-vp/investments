160aeec0dda47474f5f3eb84fed8a119031e5303
marcelo-vp <marcelos.mail@gmail.com>
2020-08-06 23:47:05 -0300

    Add babel-node to development mode and reduce bundle size

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,4 +1,7 @@
-require('core-js/es');
-require('@babel/register');
-require('dotenv').config();
+const ENV = process.env.NODE_ENV || 'development';
+
+if (ENV !== 'production') {
+    require('dotenv').config();
+}
+
 require('./server');
