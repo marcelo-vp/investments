f12427ab9d1c51bc7210fa65e58cdc1ffed7ffdb
marcelo-vp <marcelos.mail@gmail.com>
2020-08-21 13:19:15 -0300

    Add styles to data chart

--- src/frontend/components/DataChart.js
+++ src/frontend/components/DataChart.js
@@ -1,6 +1,21 @@
 import React, { Fragment } from 'react';
 import PropTypes, { object } from 'prop-types';
-import { CartesianGrid, LineChart, Line, XAxis, YAxis } from 'recharts';
+import {
+    CartesianGrid,
+    LineChart,
+    Line,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
+
+function formatDate(date) {
+    return date.split('-').reverse().join('/');
+}
+
+function formatNumber(num) {
+    return num.toFixed(2);
+}
 
 const DataChart = (props) => {
     return (
@@ -9,12 +24,25 @@ const DataChart = (props) => {
                 width={1200}
                 height={600}
                 data={props.data}
-                margin={{ top: 40, right: 40, bottom: 20, left: 20 }}
+                margin={{ top: 60, right: 40, bottom: 60, left: 40 }}
+                style={{ fontSize: 14 }}
             >
+                <XAxis
+                    dataKey='date'
+                    dy={12}
+                    minTickGap={20}
+                    tickSize={8}
+                    tickFormatter={formatDate}
+                />
+                <YAxis
+                    type='number'
+                    domain={['auto', 'auto']}
+                    dx={-6}
+                    dy={-4}
+                />
+                <Tooltip formatter={formatNumber} />
                 <CartesianGrid vertical={false} />
-                <XAxis dataKey='date' />
-                <YAxis type='number' domain={['auto', 'auto']} />
-                <Line dataKey='unitPrice' stroke='#ff7300' dot={false} />
+                <Line dataKey='unitPrice' stroke='#216ba5' dot={false} />
             </LineChart>
         </Fragment>
     );
